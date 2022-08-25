a4cca79db68eb51b09c4ce9eddea8c501d2cfe8e
Jacksongmeyer <111625641+Jacksongmeyer@users.noreply.github.com>
2022-08-25 19:02:58 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Jackson Meyer
+ *         jmeyer58@huskers.unl.edu
+ * Date: 20xx/xx/xx
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Jackson Meyer\n");
+  printf("Electrical Engineering\n");
+
+  return 0;
+}
